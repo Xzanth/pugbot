b57c0ae9f1098cceecac0dc52a0bbae7bc1760d3
Jonas Osborn <jonas.w.osborn@gmail.com>
2016-07-02 16:54:41 +0100

    Initial commit

---
+++ pugbot.rb
@@ -0,0 +1,126 @@
+require 'cinch'
+
+class Game
+	def initialize(max=10)
+		@max = max
+		@players = Array.new()
+	end
+
+	def add(name)
+		@players.push(name)
+	end
+
+	def is_player(name)
+		@players.include?(name)
+	end
+
+	def is_full()
+		if @players.length < @max
+			false
+		else
+			true
+		end
+	end
+
+	def remove(name)
+		@players.delete(name)
+	end
+
+	def update(m)
+		m.channel.topic = "[#{@players.length}/#{@max}]: #{@players.join(' ')}"
+	end
+end
+
+$game = {}
+
+bot = Cinch::Bot.new do
+	configure do |c|
+		c.nick = ""
+		c.server = ""
+		c.channels = [""]
+		c.local_host = ""
+	end
+
+	on :message, /^!start\s?(\d+)?$/ do |m, num|
+		num = num.to_i
+		if $game != {}
+			m.user.notice "Game exists, please finish current game."
+		elsif num == 0
+			$game = Game.new()
+			$game.update(m)
+		elsif num.odd?
+			m.user.notice "Game must have an even number of players."
+		elsif num > 32
+			m.user.notice "Games must have 32 or less players."
+		elsif num < 6
+			m.user.notice "Games must have at least 6 players."
+		else
+			$game = Game.new(num)
+			$game.update(m)
+		end
+	end
+	on :message, /^!add$/ do |m|
+		if $game == {}
+			m.user.notice "No game currently active."
+		elsif $game.is_full()
+			m.user.notice "This game is full, try again later!"
+		elsif $game.is_player(m.user.nick)
+			m.user.notice "You've already signed up!"
+		else
+			$game.add(m.user.nick)
+			$game.update(m)
+		end
+	end
+
+	on :message, /^!del$/ do |m|
+		if $game == {}
+			m.user.notice "No game currently active."
+		elsif not $game.is_player(m.user.nick)
+			m.user.notice "You haven't signed up!"
+		else
+			$game.remove(m.user.nick)
+			m.reply "#{m.user.nick} has abandoned us!"
+			$game.update(m)
+		end
+	end
+
+	on :message, /^!remove (.+)$/ do |m, name|
+		if not m.channel.opped?(m.user.nick)
+			m.user.notice "Access denied - must be a channel operator."
+		elsif $game == {}
+			m.user.notice "No game currently active."
+		elsif not $game.is_player(name)
+			m.user.notice "#{name} hasn't signed up!."
+		else
+			$game.remove(name)
+			m.reply "#{name} has been removed by #{m.user.nick}."
+			$game.update(m)
+		end
+	end
+
+	on :message, /^!finish$/ do |m|
+		if not m.channel.opped?(m.user.nick)
+			m.user.notice "Access denied - must be a channel operator."
+		elsif $game == {}
+			m.user.notice "No game currently active."
+		else
+			$game = {}
+			m.reply "Game ended by #{m.user.nick}."
+			m.channel.topic = ""
+		end
+	end
+
+	on :join do |m|
+		if m.user.nick == bot.nick
+			next
+		elsif $game == {}
+			m.user.notice "Welcome to #{m.channel} - no games are currently active type !start to begin signups."
+		elsif $game.is_full()
+			m.user.notice "Welcome to #{m.channel} - the next game is currently full, wait for a new game to be started and use !add to sign up."
+		else
+			m.user.notice "Welcome to #{m.channel} - signups for the next game are currently in progress, just type !add to sign up."
+		end
+	end
+end
+
+bot.start
