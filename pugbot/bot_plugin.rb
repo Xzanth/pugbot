cdb4cf7f1fea62fdd9e20ed5d92d342df884d3ea
Jonas Osborn <jonas.w.osborn@gmail.com>
2016-07-22 14:32:39 +0100

    Rewrite actual cinch bot as plugin, only skeleton so far

---
+++ pugbot/bot_plugin.rb
@@ -0,0 +1,40 @@
+module PugBot
+  # The plugin to be imported into a cinch bot instance that actually interprets
+  # the users input, tracks players and controls nearly all running of the pug
+  # bot itself.
+  class BotPlugin
+    include Cinch::Plugin
+
+    listen_to :connect, method: :setup
+    listen_to :topic,   method: :topic_changed
+    listen_to :private, method: :private_message
+    listen_to :join,    method: :joined_channel
+    listen_to :leaving, method: :left_channel
+
+    match(/help$/,                        method: :help)
+    match(/status\s?(\d+|\w+)?$/,         method: :status)
+    match(/start ([a-zA-Z]+)\s?(\d+)?$/,  method: :start)
+    match(/add\s?(\d+|\w+)?$/,            method: :add)
+    match(/del\s?(\d+|\w+)?$/,            method: :del)
+    match(/remove (.+)\s?(\d+|\w+)?$/,    method: :remove)
+    match(/end\s?(\d+|\w+)?$/,            method: :end)
+    match(/finish\s?(\d+|\w+)?$/,         method: :finish)
+    match(/sub (.+) (.+)$/,               method: :sub)
+    match(/crash$/,                       method: :shutdown)
+
+    def setup(*)
+    end
+
+    def topic_changed
+    end
+
+    def private_message
+    end
+
+    def joined_channel
+    end
+
+    def left_channel
+    end
+  end
+end
