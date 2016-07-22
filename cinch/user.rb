a610669c3550a67f7d2b2ffbe3dbae1587246434
Jonas Osborn <jonas.w.osborn@gmail.com>
2016-07-22 14:32:13 +0100

    Start using rubocop and ruby style guide, in the process moving from the one true god tabs to evil spaces

--- cinch/user.rb
+++ cinch/user.rb
@@ -1,48 +1,47 @@
-# Extending Cinch's user class to add the ability to track whether they are
-# ingame, finished or not using @status, and to handle removing them from queues
-# and other things via @track
 module Cinch
-	class User
-		# @return [Symbol] The status of the user in regards to games
-		# and queues. Can either be :standby :ingame or :finished
-		attr_accessor :status
+  # Extending Cinch's user class to add the ability to track whether they are
+  # ingame, finished or not using @status, and to handle removing them from
+  # queues and other things via @track
+  class User
+    # @return [Symbol] The status of the user in regards to games
+    # and queues. Can either be :standby :ingame or :finished
+    attr_accessor :status
 
-		# @return [Boolean] Whether we should track when this user leaves
-		# and rejoins the irc channel
-		attr_accessor :track
+    # @return [Boolean] Whether we should track when this user leaves
+    # and rejoins the irc channel
+    attr_accessor :track
 
-		# To be called whenever the user leaves the pug channel, if
-		# they are being tracked start a countdown until #timeout is
-		# called
-		# @see #timeout
-		def left
-			if @track
-				@countdown = $timers.after(120) { self.timeout }
-			end
-		end
+    # To be called whenever the user leaves the pug channel, if
+    # they are being tracked start a countdown until #timeout is
+    # called
+    # @see #timeout
+    def left
+      return unless @track
+      @countdown = $timers.after(120) { timeout }
+    end
 
-		# To be called whenever the user rejoins the pug channel, if
-		# they are being tracked, cancel the current countdown.
-		def rejoined
-			if @track
-				@countdown.cancel
-			end
-		end
+    # To be called whenever the user rejoins the pug channel, if
+    # they are being tracked, cancel the current countdown.
+    def rejoined
+      return unless @track
+      @countdown.cancel
+    end
 
-		# To be called when the timeout runs out, if the user is still
-		# being tracked, remove them from queues or if they are in game
-		# then alert that they might need to be subbed.
-		def timeout
-			if @track
-				if @status == :ingame
-					$channel.send "#{@nick} has disconnected but is in game. Please use '!sub #{@nick} new_player' to replace them if needed."
-				else
-					$channel.send "#{@nick} has not returned and has lost their space in the queue."
-					$queue_list.remove_from_queues(self)
-					@track = false
-					$queue_list.set_topic
-				end
-			end
-		end
-	end
+    # To be called when the timeout runs out, if the user is still
+    # being tracked, remove them from queues or if they are in game
+    # then alert that they might need to be subbed.
+    def timeout
+      return unless @track
+      if @status == :ingame
+        $channel.send "#{@nick} has disconnected but is in game. Please use "\
+        "'!sub #{@nick} new_player' to replace them if needed."
+      else
+        $channel.send "#{@nick} has not returned and has lost their space in "\
+        "the queue."
+        $queue_list.remove_from_queues(self)
+        @track = false
+        $queue_list.set_topic
+      end
+    end
+  end
 end
--- pugbot/game.rb
+++ pugbot/game.rb
@@ -1,54 +1,56 @@
-# This class is an object of a game actually being played, it is created when
-# a queue becomes full, given its players and a link back to the queue it came
-# from and remains until 
-class Game
-	# @return [Array<Cinch::User>] The list of users playing in this game
-	attr_reader :users
+module PugBot
+  # This class is an object of a game actually being played, it is created when
+  # a queue becomes full, given its players and a link back to the queue it came
+  # from and remains until it is ended and finished in the queue.
+  class Game
+    # @return [Array<Cinch::User>] The list of users playing in this game
+    attr_reader :users
 
-	def initialize(queue,  users)
-		@queue = queue
-		@users = users
-	end
+    def initialize(queue, users)
+      @queue = queue
+      @users = users
+    end
 
-	# Ran when the game is manually finished, change the state of all users
-	# who played and start a timer for them having to wait to join other
-	# games.
-	# @see #timeout
-	def finish
-		@users.each do |user|
-			user.status = :finished
-			user.track = false
-		end
-		$timers.after(30) { self.timeout }
-	end
+    # Ran when the game is manually finished, change the state of all users
+    # who played and start a timer for them having to wait to join other
+    # games.
+    # @see #timeout
+    def finish
+      @users.each do |user|
+        user.status = :finished
+        user.track = false
+      end
+      $timers.after(30) { timeout }
+    end
 
-	# Called after 30s have passed since this game has finished, set all the
-	# players back to standby and have all games check their waiting pools
-	# to account for all the changed statuses also remove from the queue's
-	# list of games essentially deleting the object.
-	def timeout
-		@users.each { |user| user.status = :standby }
-		$queue_list.each { |queue| queue.check_waiters }
-		@queue.finish(self)
-	end
-	
-	# Sub a currently playing user for another, changing their states and
-	# any other necessary options
-	# @param [Cinch::User] user The user currently playing to be replaced
-	# @param [Cinch::User] sub The user to be added to the game
-	def sub(user, sub)
-		@users.delete(user)
-		user.status = :standby
-		user.track = false
-		@users.push(sub)
-		sub.status = :ingame
-		sub.track = true
-		$gamelist.remove_from_queues(sub)
-	end
+    # Called after 30s have passed since this game has finished, set all the
+    # players back to standby and have all games check their waiting pools
+    # to account for all the changed statuses also remove from the queue's
+    # list of games essentially deleting the object.
+    def timeout
+      @users.each { |user| user.status = :standby }
+      $queue_list.each(&:check_waiters)
+      @queue.finish(self)
+    end
 
-	# List the current players of this game
-	# @return [String] A list of all the users
-	def to_s
-		"Current players: #{@users.join(' ')}"
-	end
+    # Sub a currently playing user for another, changing their states and
+    # any other necessary options
+    # @param [Cinch::User] user The user currently playing to be replaced
+    # @param [Cinch::User] sub The user to be added to the game
+    def sub(user, sub)
+      @users.delete(user)
+      user.status = :standby
+      user.track = false
+      @users.push(sub)
+      sub.status = :ingame
+      sub.track = true
+      $queue_list.remove_from_queues(sub)
+    end
+
+    # List the current players of this game
+    # @return [String] A list of all the users
+    def to_s
+      "Current players: #{@users.join(' ')}"
+    end
+  end
 end
