7c45f2a2c93b32f21ec31a94c5718de23f38c764
Jonas Osborn <jonas.w.osborn@gmail.com>
2016-09-06 18:15:00 +0100

    Add rate limiting back in, using CinchHttpServer class

--- lib/pugbot/web.rb
+++ lib/pugbot/web.rb
@@ -6,12 +6,20 @@ module PugBot
     include Cinch::Plugin
     extend Cinch::Plugins::HttpServer::Verbs
 
-    # use Rack::Throttle::Interval, min: 2
-    # use Rack::Throttle::Minute,   max: 30
-
     get "/api/queue_list" do
       content_type :json
       bot.pug_plugin.queue_list.to_json
+    end
+  end
+end
+
+module Cinch
+  module Plugins
+    class HttpServer
+      class CinchHttpServer
+        use Rack::Throttle::Interval, min: 2
+        use Rack::Throttle::Minute,   max: 30
+      end
     end
   end
 end
